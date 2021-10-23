4772b8f4ac5ee4e667e4ed90b57ca210a6921c57
x-Serhii.Haponenko <x-serhii.haponenko@coyote.com>
2021-10-23 15:56:11 +0300

    clean-up Console class

--- src/Console.h
+++ src/Console.h
@@ -1,19 +1,11 @@
 #ifndef _CONSOLE_H
 #define _CONSOLE_H
 
-#include <array>
+#include <Arduino.h>
 
 class Console
 {
 public:
-    static void print(const char c[]);
-    static void println();
-    static void println(const char c[]);
-    static void println(int n);
-    static void println(uint64_t n, int base);
-    static void printByteArray(uint8_t data[], size_t size);
-    template <typename T, std::size_t N> 
-    static void printByteArray(std::array<T, N>& data);
     static void printByteArray(std::string data);    
 };
 
